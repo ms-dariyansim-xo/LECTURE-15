f5e36bc4cfe88efd45e085b597dcd0f7956fdd91
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 18:51:30 -0700

    Update color.js

--- script/color.js
+++ script/color.js
@@ -6,11 +6,22 @@ const btnList = document.getElementsByClassName("btnColor");
 
 for(let i=0; i<btnList.length; it++)
 {
-    btnList[i].addEventListener('click', function(){
+    btnList[i].addEventListener('click', function()
+    {
         this.style.backgroundColor = this.value;
     })
 }
 
-display.addEventListener('click', function(){
+display.addEventListener('click', function()
+{
     this.style.backgroundColor="green";
 })
+
+btnDisplay.addEventListener('click', function()
+{
+    for(let i=o;i<t1List.length; i++){
+        t1List[i].innerText = i;
+    }
+});
+
+display.innerText = t1List.length;
