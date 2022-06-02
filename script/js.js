f10402d7571c02c65f3c011ce59c9cc0096970eb
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 18:26:54 -0700

    resetting

--- script/js.js
+++ script/js.js
@@ -1,2 +1,8 @@
 
-alert("Test to see if this works!");
+//alert("Test to see if this works!");
+const display = document.getElementById("display");
+
+display.innerHTML = "";
+display.innerText = "";
+
+//for(let i = 0; i < 10; i++){}
