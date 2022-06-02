8a6941e79f2a16dcf12f3b1476feda303882a75e
ms-dariyansim-xo <103479356+ms-dariyansim-xo@users.noreply.github.com>
2022-06-01 18:43:50 -0700

    Update color.js

---
+++ script/color.js
@@ -0,0 +1,4 @@
+const display = document.getElementById("display");
+const t1List = document.getElementsByClassName("t1");
+const btnDisplay = document.getElementById("btnDisplay");
+const btnList = document.getElementsByClassName("btnColor");
